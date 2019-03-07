6fd38cb3409f8c5034b0017847ad7962b8321821
Ravikumar M <rmittapa@uncc.edu>
2019-03-07 10:40:34 -0500

    Modified numintegration logic

--- 4.assignment-openmp-loop/4.assignment-openmp-loop/numint/numint.cpp
+++ 4.assignment-openmp-loop/4.assignment-openmp-loop/numint/numint.cpp
@@ -5,6 +5,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <chrono>
+#include <string.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -40,6 +42,183 @@ int main (int argc, char* argv[]) {
   }
 
   //insert code here
+  int functionid = atoi(argv[1]);
+  int a = atoi(argv[2]);
+  int b = atoi(argv[3]);
+  int n = atoi(argv[4]);
+  int intensity = atoi(argv[5]);
+  int nbthreads = atoi(argv[6]);
+  char *schedule_type = argv[7];
+  int granularity = atoi(argv[8]);
+  float local_xval,numintegration_sum = 0, sub_sum;
+
+  sub_sum = (((float)b - (float)a)/(float)n);
+
+  omp_set_num_threads(nbthreads); 
+
+  auto clock_start = std::chrono::system_clock::now();
+  if(strcmp(schedule_type , "static") == 0)
+  {
+    if (granularity < 0)
+    {
+      #pragma omp parallel for schedule(static) reduction(+:numintegration_sum)
+      for (int i = 0; i < n; ++i)
+      {
+        local_xval = (float)a + (((float)i + 0.5)*sub_sum);
+        switch(functionid)
+        {
+          case 1:
+            numintegration_sum += f1(local_xval,intensity);
+          break;
+          case 2:
+            numintegration_sum += f2(local_xval,intensity);
+          break;
+          case 3:
+            numintegration_sum += f3(local_xval,intensity);
+          break;
+          case 4:
+            numintegration_sum += f4(local_xval,intensity);
+          break;
+          default: std::cout<<"Please Enter correct Function ID"<<std::endl;
+        }
+      }
+    }
+    else
+    {
+      #pragma omp parallel for schedule(static,granularity) reduction(+:numintegration_sum)
+      for (int i = 0; i < n; ++i)
+      {
+        local_xval = (float)a + (((float)i + 0.5)*sub_sum);
+        switch(functionid)
+        {
+          case 1:
+            numintegration_sum += f1(local_xval,intensity);
+          break;
+          case 2:
+            numintegration_sum += f2(local_xval,intensity);
+          break;
+          case 3:
+            numintegration_sum += f3(local_xval,intensity);
+          break;
+          case 4:
+            numintegration_sum += f4(local_xval,intensity);
+          break;
+          default: std::cout<<"Please Enter correct Function ID"<<std::endl;
+        }
+      }
+    }
+  }
+  else if(strcmp(schedule_type , "dynamic") == 0) 
+  {
+    if (granularity < 0)
+    {
+      #pragma omp parallel for schedule(dynamic) reduction(+:numintegration_sum)
+      for (int i = 0; i < n; ++i)
+      {
+        local_xval = (float)a + (((float)i + 0.5)*sub_sum);
+        switch(functionid)
+        {
+          case 1:
+            numintegration_sum += f1(local_xval,intensity);
+          break;
+          case 2:
+            numintegration_sum += f2(local_xval,intensity);
+          break;
+          case 3:
+            numintegration_sum += f3(local_xval,intensity);
+          break;
+          case 4:
+            numintegration_sum += f4(local_xval,intensity);
+          break;
+          default: std::cout<<"Please Enter correct Function ID"<<std::endl;
+        }
+      }
+    }
+    else
+    {
+      #pragma omp parallel for schedule(dynamic,granularity) reduction(+:numintegration_sum)
+      for (int i = 0; i < n; ++i)
+      {
+        local_xval = (float)a + (((float)i + 0.5)*sub_sum);
+        switch(functionid)
+        {
+          case 1:
+            numintegration_sum += f1(local_xval,intensity);
+          break;
+          case 2:
+            numintegration_sum += f2(local_xval,intensity);
+          break;
+          case 3:
+            numintegration_sum += f3(local_xval,intensity);
+          break;
+          case 4:
+            numintegration_sum += f4(local_xval,intensity);
+          break;
+          default: std::cout<<"Please Enter correct Function ID"<<std::endl;
+        }
+      }
+    }
+  } 
+  else if(strcmp(schedule_type , "guided") == 0) 
+  {
+    if (granularity < 0)
+    {
+      #pragma omp parallel for schedule(guided) reduction(+:numintegration_sum)
+      for (int i = 0; i < n; ++i)
+      {
+        local_xval = (float)a + (((float)i + 0.5)*sub_sum);
+        switch(functionid)
+        {
+          case 1:
+            numintegration_sum += f1(local_xval,intensity);
+          break;
+          case 2:
+            numintegration_sum += f2(local_xval,intensity);
+          break;
+          case 3:
+            numintegration_sum += f3(local_xval,intensity);
+          break;
+          case 4:
+            numintegration_sum += f4(local_xval,intensity);
+          break;
+          default: std::cout<<"Please Enter correct Function ID"<<std::endl;
+        }
+      }
+    }
+    else
+    {
+      #pragma omp parallel for schedule(guided,granularity) reduction(+:numintegration_sum)
+      for (int i = 0; i < n; ++i)
+      {
+        local_xval = (float)a + (((float)i + 0.5)*sub_sum);
+        switch(functionid)
+        {
+          case 1:
+            numintegration_sum += f1(local_xval,intensity);
+          break;
+          case 2:
+            numintegration_sum += f2(local_xval,intensity);
+          break;
+          case 3:
+            numintegration_sum += f3(local_xval,intensity);
+          break;
+          case 4:
+            numintegration_sum += f4(local_xval,intensity);
+          break;
+          default: std::cout<<"Please Enter correct Function ID"<<std::endl;
+        }
+      }
+    }
+  }  
+  else
+  {
+    std::cout<<"Please Enter correct Sceduling Type"<<std::endl;
+  }
+
+  auto clock_end = std::chrono::system_clock::now();
+  std::chrono::duration<double>diff = clock_end - clock_start;
+  std::cout<<numintegration_sum*sub_sum<<std::endl;
+  std::cerr<<diff.count();
 
   return 0;
 }
